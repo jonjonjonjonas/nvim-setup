d077fab384eab25d7f70aeeb334066446b0d3f63
Jonas A. <jonas@jjsoftware.dk>
2026-02-06 09:01:21 +0100

    feat: opencode keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,9 +3,14 @@
 -- Add any additional keymaps here
 -- -- Map Leader (usually Space) + . to open the preview
 vim.keymap.set({ "v", "n" }, "<leader>.", require("actions-preview").code_actions)
-
 -- Move selected line / block of text in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up" })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+-- Copy relative path
+vim.keymap.set("n", "<leader>cf", ':let @+ = expand("%")<CR>', { desc = "Copy filename" })
+-- Copy absolute path
+vim.keymap.set("n", "<leader>cF", ':let @+ = expand("%:p")<CR>', { desc = "Copy full path" })
+-- Save with Cmd+S (macOS)
+vim.keymap.set({ "n", "i", "v" }, "<D-s>", "<Esc>:w<CR>", { desc = "Save file" })
